fa37d13a01b41c3ebbf2bcdab74ea005f862cc74
RoxiDron <Roksana0zed@gmail.com>
2025-07-09 20:58:32 +0300

    ДЗ сделано

--- src/Main.java
+++ src/Main.java
@@ -34,8 +34,20 @@ public class Main {
 
         System.out.println("Task 4");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i --){
-            System.out.print(reverseFullName[i]);
+        int left = 0;
+        int right = reverseFullName.length - 1;
+
+        while (left < right) {
+            char temp = reverseFullName[left];
+            reverseFullName[left] = reverseFullName[right];
+            reverseFullName[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (char c : reverseFullName) {
+            System.out.print(c);
         }
     }
 }
